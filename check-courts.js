3b02f7923a32c3b0d67ec34014c8b477c1bd7c48
puffsdad <tom@puffett.co.uk>
2026-04-14 00:16:14 +0100

    Update check-courts.js

--- check-courts.js
+++ check-courts.js
@@ -1,2 +1,2 @@
 const https = require("https");
-https.get("https://gist.githubusercontent.com/puffsdad/b53f675ad43eefafaea6356e8afae2fa/raw/fcd8c1ffb52f5f7a56231b99082b7721e14abe62/egp.js", r => { let d = ""; r.on("data", c => d += c); r.on("end", () => eval(d)); });
+https.get("https://gist.githubusercontent.com/puffsdad/b53f675ad43eefafaea6356e8afae2fa/raw/680dd0ab132882bd3c1d315bfe44991d9ac63705/egp.js", r => { let d = ""; r.on("data", c => d += c); r.on("end", () => eval(d)); });
